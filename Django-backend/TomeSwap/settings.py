d3b2ab3355da3e27a55f94b9b8ea789db5086f87
Kshitij Bhatt <kshitijbhatt1998@gmail.com>
2024-02-23 15:27:47 -0500

    Password set for server

--- Django-backend/TomeSwap/settings.py
+++ Django-backend/TomeSwap/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tomeswap',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
