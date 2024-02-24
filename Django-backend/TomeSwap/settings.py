b8f3c450473dcf6b1902b22b29cc8dec26a23921
cafreyap <cafreyap@gmail.com>
2024-02-24 06:59:17 -0500

    automatically create media folder

--- Django-backend/TomeSwap/settings.py
+++ Django-backend/TomeSwap/settings.py
@@ -141,3 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if not os.path.exists(MEDIA_ROOT):
+    os.makedirs(MEDIA_ROOT)
