ba6fb6270e3a31e86585075bc1f2c14f7a886ce7
Kshitij Bhatt <kshitijbhatt1998@gmail.com>
2024-03-21 08:36:11 -0400

    add admin

--- Django-backend/Recommendation_algorithm/apps.py
+++ Django-backend/Recommendation_algorithm/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-
-
 class RecommendationAlgorithmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Recommendation_algorithm'
