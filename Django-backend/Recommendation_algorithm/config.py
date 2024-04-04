a1bf554a721db2a9af25fbad0941e4ae80ab8b96
Kshitij Bhatt <kshitijbhatt1998@gmail.com>
2024-04-04 17:32:35 -0400

    fix

--- Django-backend/Recommendation_algorithm/config.py
+++ Django-backend/Recommendation_algorithm/config.py
@@ -4,5 +4,5 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     backend_name: str = "book-recommender-api"
-    backend_port: int = 5173
-    backend_host: str = "localhost"
+    backend_port: int = 8000
+    backend_host: str = "0.0.0.0"
