8f286a96741caf2830b346dd439d1504993ffb81
cafreyap <cafreyap@gmail.com>
2024-04-06 20:42:04 -0400

    view email

--- Django-backend/appName/urls.py
+++ Django-backend/appName/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItemViewSet
+from .views import ItemViewSet, get_user_email
 
 router = DefaultRouter()
 router.register('items', ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('get_user_email/', get_user_email, name='get_user_email'),
 ]
--- Django-backend/appName/views.py
+++ Django-backend/appName/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
 from rest_framework import viewsets
 from .models import Item
 from .serializers import ItemSerializer
@@ -8,3 +7,16 @@ from .serializers import ItemSerializer
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    
+    
+    
+def get_user_email(request):
+    # Check if the user is authenticated
+    if request.user.is_authenticated:
+        # Retrieve the email of the authenticated user
+        user_email = request.user.email
+        # Return the email as JSON response
+        return JsonResponse({'email': user_email})
+    else:
+        # Return error message if the user is not authenticated
+        return JsonResponse({'error': 'User is not authenticated'}, status=401)
